7840b480308f97d7ff73efd43d04d3b804cea89c
William_Twomey <48021997+Reilig35@users.noreply.github.com>
2021-08-15 19:25:09 +0100

    Add project files.

---
+++ Classes_and_Methods_Assignment_1/Math_Operations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_and_Methods_Assignment_1
+{
+    public class Math_Operations
+    {
+        public int OperatorbyTwo( int num1) 
+        {
+            int result = num1 * 2;
+            return result;
+        }
+        public float OperatorbyTwo( float num1) 
+        {
+            float result = num1 / 2f;
+            return result;
+        }
+        public string OperatorbyTwo( string str1) 
+        {
+            int num1 = Convert.ToInt32(str1);
+            int result = num1 - 2;
+            return result.ToString();
+        }
+    }
+}
---
+++ Classes_and_Methods_Assignment_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Classes_and_Methods_Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Method 1 Start
+            Console.WriteLine("Please type a number:\n");
+            int userInput1 = Convert.ToInt32(Console.ReadLine());
+
+            Math_Operations method1 = new Math_Operations();
+
+            int multiUser = method1.OperatorbyTwo(userInput1);
+
+            Console.WriteLine($"{userInput1} multiplied by two = {multiUser}");
+            //Method 1 End
+
+            //Method 2 Start
+            Console.WriteLine("Please type a decmal number:\n");
+            string userInput2 = Console.ReadLine();
+            float fltUser = float.Parse(userInput2);
+
+            Math_Operations method2 = new Math_Operations();
+
+            float divUser = method2.OperatorbyTwo(fltUser);
+
+            Console.WriteLine($"{userInput2} divided by 2 = {divUser}");
+            //Method 2 End
+
+            //Method 3 Start
+            Console.WriteLine("Please type a number:\n");
+            string userInput3 = Console.ReadLine();
+
+            Math_Operations method3 = new Math_Operations();
+            string subString = method3.OperatorbyTwo(userInput3);
+
+            Console.WriteLine($"{userInput3} minus 2 = {subString}");
+
+            //Method 3 End
+            
+
+        }
+    }
+}
